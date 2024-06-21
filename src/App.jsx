01cad0251b05c16925e0edd36c6dc00e13bd1ea8
Jake 'Sid' Smith <hello@jakesidsmith.com>
2024-06-21 23:15:08 +0100

    Setup routes with createBrowserRouter and Outlet for nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import NavBar from './components/NavBar';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import dataset from "./data"
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 console.log(dataset)
 
 const darkTheme = createTheme({
@@ -14,16 +15,48 @@ const darkTheme = createTheme({
   },
 });
 
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: (
+        <>
+          <Outlet />
+          <NavBar />
+        </>
+      ),
+      children: [
+        {
+          index: true,
+          element: <GameSelect />,
+        },
+        {
+          path: '/games',
+          element: <GameSelect />,
+        },
+        {
+          path: '/fighters',
+          element: <CharacterSelect />,
+        },
+        {
+          path: '/fighters/:id',
+          element: <CharacterProfile />,
+        },
+        {
+          path: '/favorites',
+          element: <p>Favorites</p>
+        }
+      ]
+    },
+  ]
+)
+
 export default function App() {
 
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-        <GameSelect />
-        {/* <CharacterSelect /> */}
-        {/* <CharacterProfile /> */}
-
-      <NavBar />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
